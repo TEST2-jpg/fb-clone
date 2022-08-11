ebb7d98f91703929ae3daf8f2cb569dc2e574acd
agapdag g <agapdag@gmail.com>
2022-08-11 17:42:42 +0800

    add prevent on multiple fetch

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,10 +1,20 @@
+import { useState } from "react"
+
 const Post = ({ postInfo, showComment }) => {
+    const [btnState, setBtn] = useState(true)
+    const preventFetch = () => {
+        setBtn(false)
+    }
     return (
-        <div key={postInfo._id}>
+        <div>
             <p>{postInfo.post}</p>
             {postInfo.comment.map((x,i) => <p key={i}>{x.comment}</p>)}
             <p>-----------------------------</p>
-            <button onClick={() => showComment(postInfo._id)}>Comment</button>
+            <button onClick={() => {
+                if (btnState) showComment(postInfo._id)
+                preventFetch()
+            }
+            }>Comment</button>
         </div>
     )
 }
