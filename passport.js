25163e58fcda60c55077f4ed08fc627794bf7265
agapdag g <agapdag@gmail.com>
2022-08-13 15:27:18 +0800

    added jwt verification

--- passport.js
+++ passport.js
@@ -1,7 +1,14 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user')
 const bcrypt = require('bcrypt')
+const passportJWT = require("passport-jwt");
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
     usernameField: 'email',
@@ -14,6 +21,18 @@ passport.use(new LocalStrategy({
             return done(null, user, { message: 'Logged In Successfully' })
         }
         return done(null, false, { message: 'Incorrect email or password.' })
+    } catch (error) {
+        return done(error)
+    }
+}
+));
+
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromUrlQueryParameter('token'),
+    secretOrKey : process.env.JWT_SECRET
+}, async (jwtPayload, done) => {
+    try {
+        return done(null, jwtPayload._id)
     } catch (error) {
         return done(error)
     }
