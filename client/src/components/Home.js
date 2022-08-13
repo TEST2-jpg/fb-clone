845b0759025afa059ce4a982f869ba16f505b076
agapdag g <agapdag@gmail.com>
2022-08-13 16:00:38 +0800

    Add sidebar and flex container

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react'
 import Post from './Post'
-
+import CreatePost from './CreatePost'
+import Sidebar from './Sidebar'
 const Home = () => {
     const [feed, setFeed] = useState('')
     useEffect(() => {
-        const fetchPost = async() => {
+        const fetchPost = async () => {
             const response = await fetch('http://localhost:8080')
             const postData = await response.json()
             setFeed(postData.postList)
@@ -18,16 +19,16 @@ const Home = () => {
             console.log(feed, 'feed')
             let updatedPost = feed.map(x => {
                 if (x._id === postId) {
-                    return { ...x, comment: commentArray.comment}
+                    return { ...x, comment: commentArray.comment }
                 }
                 return x
             })
             setFeed(updatedPost)
         }
-        const fetchThisComment = async() => {
-            const response = await fetch('http://localhost:8080/users/'+ postId)
+        const fetchThisComment = async () => {
+            const response = await fetch('http://localhost:8080/users/' + postId)
             const postData = await response.json()
-            console.log(postData, 'POSTDAATA') 
+            console.log(postData, 'POSTDAATA')
             replace(postData)
         }
         fetchThisComment().catch(console.error)
@@ -38,11 +39,18 @@ const Home = () => {
             <button onClick={() => {
                 console.log(feed)
             }}>CONSOLE LOGS</button>
-           {feed ? feed.map((postInfo) => {
-                return(
-                    <Post postInfo={postInfo} key={postInfo._id} showComment={showComment}/>
-                )
-           }): <h1>Loading</h1>}
+            <div className='flex-container'>
+                <Sidebar />
+                <div>
+                    <CreatePost />
+                    {feed ? feed.map((postInfo) => {
+                        return (
+                            <Post postInfo={postInfo} key={postInfo._id} showComment={showComment} />
+                        )
+                    }) : <h1>Loading</h1>}
+                </div>
+            </div>
+
         </div>
     )
 }
