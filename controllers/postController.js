1045adbad38e300c669e181b1e4cfcb51f1bfbb5
agapdag g <agapdag@gmail.com>
2022-08-14 15:41:27 +0800

    Add post deletion

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,23 +2,24 @@ const Post = require('../models/post')
 const Comment = require('../models/comment')
 const User = require('../models/user')
 
+
 exports.getPostsComment = async (req, res, next) => {
     const postId = req.params.postId
     try {
         const post = await Post.findById(postId)
-        const commentsPop = await post.populate({path: 'comment', model: Comment})
-        await res.status(200).json({comment: commentsPop.comment})
+        const commentsPop = await post.populate({ path: 'comment', model: Comment })
+        await res.status(200).json({ comment: commentsPop.comment })
     } catch (error) {
-        if(error) return next(error)
+        if (error) return next(error)
     }
 };
 
-exports.createPost = async(req, res, next) => {
-    const {postBody, userId} = req.body
+exports.createPost = async (req, res, next) => {
+    const { postBody, userId } = req.body
     const user = await User.findById(userId)
-    const post = await new Post({
+    const post = new Post({
         post: postBody,
-        author : user._id
+        author: user._id
     })
     await post.save()
     user.posts.push(post)
@@ -29,3 +30,16 @@ exports.createPost = async(req, res, next) => {
         message: 'Post created successfully!',
     });
 };
+
+exports.deletePost = async (req, res, next) => {
+    const { userId, postId } = req.params
+    Post.findById(postId)
+        .populate('author', '_id')
+        .exec(function (err, post) {
+            if (err) return next(err);
+            if (userId !== post.author._id.toString()) {
+                res.status(401).json({message: 'Unauthorized'})
+            }
+            Post.findByIdAndRemove(postId, () => res.status(200).json({message:'deleted'}))
+        });
+}
