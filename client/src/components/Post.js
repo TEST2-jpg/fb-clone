05d3cb96acffe84887a096f45fbc10b0b7824a62
agapdag g <agapdag@gmail.com>
2022-08-14 15:45:50 +0800

    Add delete request

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,10 +1,21 @@
 import { useState } from "react"
 
-const Post = ({ postInfo, showComment }) => {
+const Post = ({ postInfo, showComment, userId, token, loadFeed }) => {
     const [btnState, setBtn] = useState(true)
     const preventFetch = () => {
         setBtn(false)
     }
+    const deletePost = async(postId) => {
+        await fetch(`http://localhost:8080/users/${userId}/posts/${postId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                Authorization: 'Bearer ' + token
+            }
+        })
+        console.log('run')
+    }
+
     return (
         <div>
             <p><b>{`${postInfo.author.first_name} ${postInfo.author.last_name}`}</b></p>
@@ -16,6 +27,10 @@ const Post = ({ postInfo, showComment }) => {
                 preventFetch()
             }
             }>Comment</button>
+            {postInfo.author._id === userId ? <button onClick={ async ()=>{
+                await deletePost(postInfo._id)
+                loadFeed()
+            }}>Delete</button>: null}
         </div>
     )
 }
