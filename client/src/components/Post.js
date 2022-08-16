f73fc282b7613ab1df67ed8e8baf497c10656f9e
agapdag g <agapdag@gmail.com>
2022-08-16 16:55:45 +0800

    Add fetch and render of comment number

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,11 +1,16 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { ReactComponent as Option } from '../assets/options.svg'
+import { ReactComponent as Profile } from '../assets/profile.svg'
+import WriteComment from "./WriteComment"
+
 
 const Post = ({ postInfo, showComment, userId, token, loadFeed }) => {
     const [btnState, setBtn] = useState(true)
+    const [postStat, setpostStat] = useState([])
     const preventFetch = () => {
         setBtn(false)
     }
-    const deletePost = async(postId) => {
+    const deletePost = async (postId) => {
         await fetch(`http://localhost:8080/users/${userId}/posts/${postId}`, {
             method: 'DELETE',
             headers: {
@@ -15,22 +20,56 @@ const Post = ({ postInfo, showComment, userId, token, loadFeed }) => {
         })
         console.log('run')
     }
+    const getPostStat = async (postId) => {
+        try {
+            const response = await fetch(`http://localhost:8080/users/posts/${postId}/postStat`, {
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            })
+            const data = await response.json()
+            setpostStat(data.comments.length)
+            return data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getPostStat(postInfo._id)
+    }, [])
 
     return (
-        <div>
-            <p><b>{`${postInfo.author.first_name} ${postInfo.author.last_name}`}</b></p>
-            <p>{postInfo.post}</p>
-            {postInfo.comment.map((x,i) => <p key={i}>{x.comment}</p>)}
-            <p>-----------------------------</p>
-            <button onClick={() => {
+        <div className="container border bg-white rounded-4 post-width mb-3">
+            <div className="d-flex post-head">
+                <div className="d-flex w-100 align-items-center">
+                    <span><Profile className='profile' /></span>
+                    <div className="flex-container-column flex-grow-1">
+                        <h2 className="author-name">{`${postInfo.author.first_name} ${postInfo.author.last_name}`}</h2>
+                        <div className="created-at">22h</div>
+                    </div>
+                    <Option />
+                </div>
+            </div>
+            <div className='px-3 fs-6 font-color'>{postInfo.post}</div>
+            {postStat !== 0 ? <div className="px-3"> {postStat > 1 ? <p>{postStat} comments</p>: <p>{postStat} comment</p>}</div> : null}
+            <div className="border-top border-bottom d-flex justify-content-around p-2">
+                <button className="rounded flex-fill" onClick={() => {
+                    if (btnState) showComment(postInfo._id)
+                    preventFetch()
+                }
+                }>Comment</button>
+                {postInfo.author._id === userId ? <button className="flex-fill rounded" onClick={async () => {
+                    await deletePost(postInfo._id)
+                    loadFeed()
+                }}>Delete</button> : null}
+            </div>
+            <WriteComment showComment={showComment} userId={userId} authorId={postInfo.author._id} token={token} postInfoId={postInfo._id} />
+            {postInfo.comment.map((x, i) => <div key={i}> {x.author.first_name} {x.author.last_name} ---- {x.comment}</div>)}
+            <div onClick={() => {
                 if (btnState) showComment(postInfo._id)
                 preventFetch()
-            }
-            }>Comment</button>
-            {postInfo.author._id === userId ? <button onClick={ async ()=>{
-                await deletePost(postInfo._id)
-                loadFeed()
-            }}>Delete</button>: null}
+            }}>View more comments</div>
         </div>
     )
 }
