9f77e1174d3ee565b9ab2b533d3acc31223b17ae
agapdag g <agapdag@gmail.com>
2022-08-16 14:36:15 +0800

    Update query

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,15 +4,18 @@ const User = require('../models/user')
 exports.getUserInfo = async (req, res, next) => {
     const userId = req.params.userId
     const user = await User.findById(userId)
-    const {_id, first_name, last_name, email} = user
-    await res.status(200).json({_id, first_name, last_name, email})
+    const { _id, first_name, last_name, email } = user
+    await res.status(200).json({ _id, first_name, last_name, email })
 }
 
 exports.getPosts = async (req, res, next) => {
-    Post.find().populate({path:'author', select: ['first_name', 'last_name']}).exec((err, postList) => {
-        if (err) return next(err);
-        res.status(200).json({message: 'Post list fetched', postList})
-    })
+    Post.find().sort({ createdAt: -1 })
+        .populate({ path: 'author', select: ['first_name', 'last_name'] })
+        .populate({ path: 'comment', options: { limit: 2, sort: { 'createdAt': -1 } }, populate: {path: 'author' , select: ['first_name', 'last_name']} })
+        .exec((err, postList) => {
+            if (err) return next(err);
+            res.status(200).json({ message: 'Post list fetched', postList })
+        })
 };
 
 
