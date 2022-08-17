9970a93515cd565b671fa57f2c940d7ff60cfdf1
agapdag g <agapdag@gmail.com>
2022-08-17 16:42:42 +0800

    Add stat route

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,7 +6,7 @@ exports.getPostsComment = async (req, res, next) => {
     const postId = req.params.postId
     try {
         const post = await Post.findById(postId)
-        const commentsPop = await post.populate({ path: 'comment', populate: {path: 'author' , select: ['first_name', 'last_name']}})
+        const commentsPop = await post.populate({ path: 'comment', populate: {path: 'author' , select: ['first_name', 'last_name']}, options: { sort: { 'createdAt': -1 } }})
         await res.status(200).json({ comment: commentsPop.comment })
     } catch (error) {
         if (error) return next(error)
@@ -52,4 +52,13 @@ exports.postComment = async (req, res, next) => {
     await comment.save()
     await post.save()
     res.status(200).json({message: 'in postcomment post'})
+}
+
+exports.postStats = async (req, res, next) => {
+    const postId = req.params.postId
+    Post.findById(postId).distinct('comment', (err, comments) => {
+        if (err) return next(err)
+        console.log(comments)
+        res.status(201).json({postId, comments})
+    })
 }
