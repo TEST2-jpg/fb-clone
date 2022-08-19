81e6195bf07a270f0de7bdfd50cc21ecdcbca8f5
agapdag g <agapdag@gmail.com>
2022-08-19 17:01:49 +0800

    add remove request

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -3,15 +3,15 @@ const Friend = require('../models/friend')
 
 exports.addFriend = async (req, res, next) => {
     console.log(req.body)
-    const {requesterId, recipientId} = req.body
+    const { requesterId, recipientId } = req.body
     const requesterStatus = new Friend({
-        requester : requesterId,
-        recipient : recipientId,
+        requester: requesterId,
+        recipient: recipientId,
         status: 1,
     })
     const recipientStatus = new Friend({
-        recipient : requesterId,
-        requester : recipientId,
+        recipient: requesterId,
+        requester: recipientId,
         status: 2
     })
     await requesterStatus.save()
@@ -25,31 +25,49 @@ exports.addFriend = async (req, res, next) => {
     recipient.friends.push(recipientStatus)
     await recipient.save()
 
-    res.status(200).json({message: 'request sent', requester})
+    res.status(200).json({ message: 'request sent', requester })
 }
 
-exports.getAddFriendStatus = async(req, res, next) => {
-    const {uId, friendId} = req.params
-    const user = await User.findById(uId).populate({path: 'friends', match: {recipient: friendId} })
-    if(user.friends.length === 0) return res.status(200).json({message: 'Add friend', friendStatus:[{status: 0}]})
-    console.log(user.friends, 'oooooooo')
-    res.status(200).json({message: 'fetched', friendStatus: user.friends})
+exports.getAddFriendStatus = async (req, res, next) => {
+    const { uId, friendId } = req.params
+    const user = await User.findById(uId).populate({ path: 'friends', match: { recipient: friendId } })
+    if (user.friends.length === 0) return res.status(200).json({ message: 'Add friend', friendStatus: [{ status: 0 }] })
+    res.status(200).json({ message: 'fetched', friendStatus: user.friends })
 }
 
-exports.undoRequest = async(req, res, next) => {
-    const {requesterId, recipientId} = req.body
-    
-    const requesterStatus = await Friend.deleteMany({
+exports.undoRequest = async (req, res, next) => {
+    const { requesterId, recipientId } = req.body
+
+    const requesterStatus = await Friend.findOne({
         requester: requesterId,
         recipient: recipientId
     })
 
-    const recipientStatus = await Friend.deleteMany({
+    const recipientStatus = await Friend.findOne({
+        recipient: requesterId,
+        requester: recipientId,
+    })
+
+    const requester = await User.findById(requesterId)
+    const recipient = await User.findById(recipientId)
+
+    requester.friends.pull({ _id: requesterStatus._id })
+    recipient.friends.pull({ _id: recipientStatus._id })
+
+    await requester.save()
+    await recipient.save()
+    await recipientStatus.save()
+    await recipientStatus.save()
+
+    await Friend.deleteOne({
+        requester: requesterId,
+        recipient: recipientId
+    })
+
+    await Friend.deleteOne({
         recipient: requesterId,
         requester: recipientId
     })
 
-    console.log(requesterStatus ,'11111111111111111111111111', recipientStatus)
-
-    res.status(200).json({message: 'Done'})
+    res.status(200).json({ message: 'Done' })
 }
