0359bb0b61408b55dab9b71fed1264f79d6851a3
agapdag g <agapdag@gmail.com>
2022-08-20 18:45:39 +0800

    Add like and unlike routes

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,21 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/posts/:postId/postStat', postController.postStats)
+
 router.post('/:userId/posts/', postController.createPost)
 
 router.get('/:userId', homeController.getUserInfo)
 
-// router.delete('/:userId/posts/:postId', postController.deletePost)
+router.delete('/:userId/posts/:postId', postController.deletePost)
 
 router.get('/:userId/posts/:postId', postController.getPostsComment)
+
+router.post('/:userId/posts/:postId/comments', postController.postComment)
+
+router.get('/:userId/posts/:postId/like', postController.likePost)
+
+router.get('/:userId/posts/:postId/unlike', postController.unlikePost)
 
 
 module.exports = router;
