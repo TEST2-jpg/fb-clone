f1723d4c176df69827a197cceefc7cc85555fa5e
agapdag g <agapdag@gmail.com>
2022-08-20 18:46:09 +0800

    Add like and unlike post controls

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,7 +6,7 @@ exports.getPostsComment = async (req, res, next) => {
     const postId = req.params.postId
     try {
         const post = await Post.findById(postId)
-        const commentsPop = await post.populate({ path: 'comment', populate: {path: 'author' , select: ['first_name', 'last_name']}, options: { sort: { 'createdAt': -1 } }})
+        const commentsPop = await post.populate({ path: 'comment', populate: { path: 'author', select: ['first_name', 'last_name'] }, options: { sort: { 'createdAt': -1 } } })
         await res.status(200).json({ comment: commentsPop.comment })
     } catch (error) {
         if (error) return next(error)
@@ -23,8 +23,7 @@ exports.createPost = async (req, res, next) => {
     await post.save()
     user.posts.push(post)
     await user.save()
-    console.log(user)
-    console.log(post, userId)
+
     res.status(201).json({
         message: 'Post created successfully!',
     });
@@ -37,28 +36,55 @@ exports.deletePost = async (req, res, next) => {
         .exec(function (err, post) {
             if (err) return next(err);
             if (userId !== post.author._id.toString()) {
-                res.status(401).json({message: 'Unauthorized'})
+                res.status(401).json({ message: 'Unauthorized' })
             }
-            Post.findByIdAndRemove(postId, () => res.status(200).json({message:'deleted'}))
+            Post.findByIdAndRemove(postId, () => res.status(200).json({ message: 'deleted' }))
         });
 }
 
 exports.postComment = async (req, res, next) => {
-    const {postId} = req.params
-    const {commentBody, userId} = req.body
+    const { postId } = req.params
+    const { commentBody, userId } = req.body
     const post = await Post.findById(postId)
-    const comment = new Comment({comment: commentBody, author: userId})
+    const comment = new Comment({ comment: commentBody, author: userId })
     post.comment.push(comment)
     await comment.save()
     await post.save()
-    res.status(200).json({message: 'in postcomment post'})
+    res.status(200).json({ message: 'in postcomment post' })
 }
 
 exports.postStats = async (req, res, next) => {
     const postId = req.params.postId
     Post.findById(postId).distinct('comment', (err, comments) => {
         if (err) return next(err)
-        console.log(comments)
-        res.status(201).json({postId, comments})
+        res.status(201).json({ postId, comments })
     })
 }
+
+exports.likePost = async (req, res, next) => {
+    const {postId, userId} = req.params
+    const post = await Post.findOneAndUpdate({
+        _id: postId
+    }, {$inc : {'likes' : 1}})
+    
+    post.likers.push(userId)
+
+    await post.save()
+
+    res.status(200).json({ message: 'liked', postId, post})
+
+}
+
+exports.unlikePost = async (req, res, next) => {
+    const {postId, userId} = req.params
+    const post = await Post.findOneAndUpdate({
+        _id: postId
+    }, {$inc : {'likes' : -1}})
+    
+    post.likers.pull(userId)
+
+    await post.save()
+
+    res.status(200).json({ message: 'unliked', postId, post})
+
+} 
