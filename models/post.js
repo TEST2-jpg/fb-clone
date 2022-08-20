d10ecbb08f89e975a827e6f5c33f0e8e95c360de
agapdag g <agapdag@gmail.com>
2022-08-20 18:45:15 +0800

    Update post model

--- models/post.js
+++ models/post.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const PostSchema = new Schema ({
-    caption: {type: String},
-    post: {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, ref: 'User'},
-    comment: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
-})
+const PostSchema = new Schema({
+    caption: { type: String },
+    post: { type: String, required: true },
+    likes: {type: Number},
+    likers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    comment: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+
+}, { timestamps: true })
 
 
 module.exports = mongoose.model('Post', PostSchema);
