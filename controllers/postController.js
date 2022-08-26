30c8d978a5d4987eecdcdbf2cf59dc91986c912c
agapdag g <agapdag@gmail.com>
2022-08-26 18:25:53 +0800

    Add validators

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,109 +1,138 @@
-const Post = require('../models/post')
-const Comment = require('../models/comment')
-const User = require('../models/user')
+const Post = require("../models/post");
+const Comment = require("../models/comment");
+const User = require("../models/user");
+const { body, validationResult } = require("express-validator");
 
 exports.getPostsComment = async (req, res, next) => {
-    const postId = req.params.postId
-    try {
-        const post = await Post.findById(postId)
-        const commentsPop = await post.populate({ path: 'comment', populate: { path: 'author', select: ['first_name', 'last_name'] }, options: { sort: { 'createdAt': -1 } } })
-        await res.status(200).json({ comment: commentsPop.comment })
-    } catch (error) {
-        if (error) return next(error)
-    }
+  const postId = req.params.postId;
+  try {
+    const post = await Post.findById(postId);
+    const commentsPop = await post.populate({
+      path: "comment",
+      populate: { path: "author", select: ["first_name", "last_name"] },
+      options: { sort: { createdAt: -1 } },
+    });
+    await res.status(200).json({ comment: commentsPop.comment });
+  } catch (error) {
+     return next(error);
+  }
 };
 
-exports.createPost = async (req, res, next) => {
-    const { postBody, userId } = req.body
-    const user = await User.findById(userId)
-    const post = new Post({
+exports.createPost = [
+  body("postBody", "post must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(422)
+        .json({ message: "Validation failed", error: errors.msg });
+    }
+    const { postBody, userId } = req.body;
+    try {
+      const user = await User.findById(userId);
+      const post = new Post({
         post: postBody,
-        author: user._id
-    })
-    await post.save()
-    user.posts.push(post)
-    await user.save()
+        author: user._id,
+      });
+      await post.save();
+      user.posts.push(post);
+      await user.save();
+    } catch (error) {
+        return next(error)
+    }
 
     res.status(201).json({
-        message: 'Post created successfully!',
+      message: "Post created successfully!",
+    });
+  },
+];
+
+exports.deletePost = async (req, res, next) => {
+  const { userId, postId } = req.params;
+  Post.findById(postId)
+    .populate("author", "_id")
+    .exec(function (err, post) {
+      if (err) return next(err);
+      if (userId !== post.author._id.toString()) {
+        res.status(401).json({ message: "Unauthorized" });
+      }
+      Post.findByIdAndRemove(postId, () =>
+        res.status(200).json({ message: "deleted" })
+      );
     });
 };
 
-exports.deletePost = async (req, res, next) => {
-    const { userId, postId } = req.params
-    Post.findById(postId)
-        .populate('author', '_id')
-        .exec(function (err, post) {
-            if (err) return next(err);
-            if (userId !== post.author._id.toString()) {
-                res.status(401).json({ message: 'Unauthorized' })
-            }
-            Post.findByIdAndRemove(postId, () => res.status(200).json({ message: 'deleted' }))
-        });
-}
-
 exports.postComment = async (req, res, next) => {
-    const { postId } = req.params
-    const { commentBody, userId } = req.body
-    const post = await Post.findById(postId)
-    const comment = new Comment({ comment: commentBody, author: userId })
-    post.comment.push(comment)
-    await comment.save()
-    await post.save()
-    res.status(200).json({ message: 'in postcomment post' })
-}
+  const { postId } = req.params;
+  const { commentBody, userId } = req.body;
+  const post = await Post.findById(postId);
+  const comment = new Comment({ comment: commentBody, author: userId });
+  post.comment.push(comment);
+  await comment.save();
+  await post.save();
+  res.status(200).json({ message: "in postcomment post" });
+};
 
 exports.postStats = async (req, res, next) => {
-    const postId = req.params.postId
-    Post.findById(postId).distinct('comment', (err, comments) => {
-        if (err) return next(err)
-        res.status(201).json({ postId, comments })
-    })
-}
+  const postId = req.params.postId;
+  Post.findById(postId).distinct("comment", (err, comments) => {
+    if (err) return next(err);
+    res.status(201).json({ postId, comments });
+  });
+};
 
 exports.likePost = async (req, res, next) => {
-    const { postId, userId } = req.params
-    const post = await Post.findOneAndUpdate({
-        _id: postId
-    }, { $inc: { 'likes': 1 } })
+  const { postId, userId } = req.params;
+  const post = await Post.findOneAndUpdate(
+    {
+      _id: postId,
+    },
+    { $inc: { likes: 1 } }
+  );
 
-    post.likers.push(userId)
+  post.likers.push(userId);
 
-    await post.save()
+  await post.save();
 
-    res.status(200).json({ message: 'liked', postId, post })
-
-}
+  res.status(200).json({ message: "liked", postId, post });
+};
 
 exports.unlikePost = async (req, res, next) => {
-    const { postId, userId } = req.params
-    const post = await Post.findOneAndUpdate({
-        _id: postId
-    }, { $inc: { 'likes': -1 } })
+  const { postId, userId } = req.params;
+  const post = await Post.findOneAndUpdate(
+    {
+      _id: postId,
+    },
+    { $inc: { likes: -1 } }
+  );
 
-    post.likers.pull(userId)
+  post.likers.pull(userId);
 
-    await post.save()
+  await post.save();
 
-    res.status(200).json({ message: 'unliked', postId, post })
-
-}
+  res.status(200).json({ message: "unliked", postId, post });
+};
 
 exports.getPostInfo = async (req, res, next) => {
-    const { postId } = req.params
-    const post = await Post.findById(postId)
-    const postMessage = post.post
-    res.status(200).json({ message: 'Received', postId, postMessage })
-
-}
+  const { postId } = req.params;
+  const post = await Post.findById(postId);
+  const postMessage = post.post;
+  res.status(200).json({ message: "Received", postId, postMessage });
+};
 
 exports.editPost = async (req, res, next) => {
-    const { postId } = req.params
-    const { postBody } = req.body
-    const post = await Post.findOneAndUpdate({ _id: postId }, { post: postBody }, {
-        new: true
-    })
+  const { postId } = req.params;
+  const { postBody } = req.body;
+  const post = await Post.findOneAndUpdate(
+    { _id: postId },
+    { post: postBody },
+    {
+      new: true,
+    }
+  );
 
-    res.status(200).json({ message: 'Edited', postBody, post })
-
-} 
+  res.status(200).json({ message: "Edited", postBody, post });
+};
