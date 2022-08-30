c26d5ddf61604d6af8cdaf0e4a20565c08099ab4
agapdag g <agapdag@gmail.com>
2022-08-30 13:44:26 +0800

    Add imageUrl

--- models/post.js
+++ models/post.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
+const PostSchema = new Schema(
+  {
     caption: { type: String },
-    post: { type: String, required: true },
-    likes: {type: Number},
-    likers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    author: { type: Schema.Types.ObjectId, ref: 'User' },
-    comment: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    post: { type: String },
+    imageUrl: { type: String },
+    likes: { type: Number },
+    likers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    comment: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  },
+  { timestamps: true }
+);
 
-}, { timestamps: true })
-
-
-module.exports = mongoose.model('Post', PostSchema);
+module.exports = mongoose.model("Post", PostSchema);
