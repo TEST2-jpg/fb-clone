e2a044eec8a090c9f5717532c08d827789f9fd1b
agapdag g <agapdag@gmail.com>
2022-08-31 20:17:29 +0800

    Add author and timestamp

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema
 
 const CommentSchema = new Schema ({
     comment: {type: String, required: true},
-})
+    author: {type: Schema.Types.ObjectId, ref: 'User'}
+},{ timestamps: true })
 
 
 module.exports = mongoose.model('Comment', CommentSchema);
