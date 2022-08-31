1e6c0a9b2a5b468df8c8b0b0acc5ba316bcc0fbe
agapdag g <agapdag@gmail.com>
2022-08-31 20:20:53 +0800

    Add image methods

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,33 +2,33 @@ const Post = require("../models/post");
 const Comment = require("../models/comment");
 const User = require("../models/user");
 const { body, validationResult } = require("express-validator");
+const { cloudinary } = require("../cloudinary");
 
 exports.getPostsComment = async (req, res, next) => {
   const postId = req.params.postId;
+  const commentNum = req.params.skip;
   try {
     const post = await Post.findById(postId);
     const commentsPop = await post.populate({
       path: "comment",
       populate: { path: "author", select: ["first_name", "last_name"] },
-      options: { sort: { createdAt: -1 } },
+      options: { sort: { createdAt: -1 }, skip: commentNum, limit: 5 },
     });
     await res.status(200).json({ comment: commentsPop.comment });
   } catch (error) {
-     return next(error);
+    return next(error);
   }
 };
 
 exports.createPost = [
-  body("postBody", "post must not be empty")
-    .trim()
-    .isLength({ min: 1 })
-    .escape(),
+  body("postBody", "post must not be empty").trim(),
   async (req, res, next) => {
     const errors = validationResult(req);
+    console.log(req.file);
     if (!errors.isEmpty()) {
       return res
         .status(422)
-        .json({ message: "Validation failed", error: errors.msg });
+        .json({ message: "Validation failed", errors: errors.array() });
     }
     const { postBody, userId } = req.body;
     try {
@@ -36,17 +36,19 @@ exports.createPost = [
       const post = new Post({
         post: postBody,
         author: user._id,
+        ...(req.file && { imageUrl: req.file.path }),
+        ...(req.file && { imageId: req.file.filename }),
       });
       await post.save();
       user.posts.push(post);
       await user.save();
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: post,
+      });
     } catch (error) {
-        return next(error)
+      return next(error);
     }
-
-    res.status(201).json({
-      message: "Post created successfully!",
-    });
   },
 ];
 
@@ -55,26 +57,46 @@ exports.deletePost = async (req, res, next) => {
   Post.findById(postId)
     .populate("author", "_id")
     .exec(function (err, post) {
-      if (err) return next(err);
+      if (err || !post) return next(err);
       if (userId !== post.author._id.toString()) {
         res.status(401).json({ message: "Unauthorized" });
       }
-      Post.findByIdAndRemove(postId, () =>
-        res.status(200).json({ message: "deleted" })
-      );
+      Post.findByIdAndRemove(postId, async () => {
+        if (!post.imageUrl) return res.status(200).json({ message: "Deleted" });
+        try {
+          await cloudinary.uploader.destroy(post.imageId);
+          res.status(200).json({ message: "deleted" });
+        } catch (error) {
+          return next(error);
+        }
+      });
     });
 };
 
-exports.postComment = async (req, res, next) => {
-  const { postId } = req.params;
-  const { commentBody, userId } = req.body;
-  const post = await Post.findById(postId);
-  const comment = new Comment({ comment: commentBody, author: userId });
-  post.comment.push(comment);
-  await comment.save();
-  await post.save();
-  res.status(200).json({ message: "in postcomment post" });
-};
+exports.postComment = [
+  body("commentBody", "comment must not be empty").trim().isLength({ min: 1 }),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(422)
+        .json({ message: "Validation failed", error: errors.array() });
+    }
+    try {
+      const { postId } = req.params;
+      const { commentBody, userId } = req.body;
+      const post = await Post.findById(postId);
+      const comment = new Comment({ comment: commentBody, author: userId });
+      post.comment.push(comment);
+      await comment.save();
+      await post.save();
+      const popcomment = await comment.populate("author");
+      res.status(200).json({ comment: popcomment });
+    } catch (error) {
+      return next(error);
+    }
+  },
+];
 
 exports.postStats = async (req, res, next) => {
   const postId = req.params.postId;
@@ -117,22 +139,30 @@ exports.unlikePost = async (req, res, next) => {
 };
 
 exports.getPostInfo = async (req, res, next) => {
-  const { postId } = req.params;
-  const post = await Post.findById(postId);
-  const postMessage = post.post;
-  res.status(200).json({ message: "Received", postId, postMessage });
+  try {
+    const { postId } = req.params;
+    const post = await Post.findById(postId);
+    const postMessage = post.post;
+    res.status(200).json({ message: "Received", postId, postMessage });
+  } catch (error) {
+    return next(error);
+  }
 };
 
 exports.editPost = async (req, res, next) => {
   const { postId } = req.params;
   const { postBody } = req.body;
-  const post = await Post.findOneAndUpdate(
-    { _id: postId },
-    { post: postBody },
-    {
-      new: true,
-    }
-  );
+  try {
+    const post = await Post.findOneAndUpdate(
+      { _id: postId },
+      { post: postBody },
+      {
+        new: true,
+      }
+    );
 
-  res.status(200).json({ message: "Edited", postBody, post });
+    res.status(200).json({ message: "Edited", postBody, post });
+  } catch (error) {
+    return next(error);
+  }
 };
