f6762f69bd9a4081ecded8e4e661c368e8c9e0ee
agapdag g <agapdag@gmail.com>
2022-09-03 18:57:14 +0800

    Add infinite scrolling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,87 +1,149 @@
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Post from './Post'
-import CreatePost from './CreatePost'
-import Sidebar from './Sidebar'
-import Login from './Login'
-import Contacts from './Contacts'
-import Nav from './Nav'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Post from "./Post";
+import CreatePost from "./CreatePost";
+import Sidebar from "./Sidebar";
+import Login from "./Login";
+import Contacts from "./Contacts";
+import Nav from "./Nav";
+import { ReactComponent as Profile } from "../assets/profile.svg";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 const Home = ({ token, ps, userData, userId }) => {
-    const [feed, setFeed] = useState('')
-    const navigate = useNavigate()
-    const loadFeed = () => {
-        const fetchPost = async () => {
-            const response = await fetch('http://localhost:8080', {
-                headers: { Authorization: 'Bearer ' + token }
-            })
-            const postData = await response.json()
-            setFeed(postData.postList)
-            console.log(postData)
+  const [feed, setFeed] = useState([]);
+  const [postModal, setPostModal] = useState(false);
+  const navigate = useNavigate();
+  const loadFeed = () => {
+    const fetchPost = async () => {
+      const response = await fetch(
+        `http://localhost:8080?skip=${feed.length}`,
+        {
+          headers: { Authorization: "Bearer " + token },
         }
-        fetchPost().catch(console.error)
+      );
+      const postData = await response.json();
+      setFeed(postData.postList);
+      console.log(postData);
+    };
+    fetchPost().catch(console.error);
+  };
+
+  useEffect(() => {
+    if (!token) {
+      return navigate("/login");
     }
+    loadFeed();
+  }, []);
 
-    useEffect(() => {
-        if (!token) {
-            return navigate('/login')
+  const showComment = (postId, skip) => {
+    const replace = (commentArray) => {
+      let updatedPost = feed.map((x) => {
+        if (x._id === postId) {
+          return { ...x, comment: [...x.comment, ...commentArray.comment] };
         }
-    }, [token])
-
-    useEffect(() => {
-        loadFeed()
-    }, [])
-
-    const showComment = (postId) => {
-        const replace = (commentArray) => {
-            console.log(feed, 'feed')
-            let updatedPost = feed.map(x => {
-                if (x._id === postId) {
-                    return { ...x, comment: commentArray.comment }
-                }
-                return x
-            })
-            setFeed(updatedPost)
+        return x;
+      });
+      setFeed(updatedPost);
+    };
+    const fetchThisComment = async () => {
+      const response = await fetch(
+        `http://localhost:8080/users/${userData._id}/posts/${postId}/comments?skip=${skip}`,
+        {
+          headers: { Authorization: "Bearer " + token },
         }
-        const fetchThisComment = async () => {
-            const response = await fetch(`http://localhost:8080/users/${userData._id}/posts/${postId}/comments`, {
-                headers: { Authorization: 'Bearer ' + token }
-            })
-            const postData = await response.json()
-            console.log(postData, 'POSTDAATA')
-            replace(postData)
-        }
-        fetchThisComment().catch(console.error)
-    }
-    return (
-        <>
-            {token ? <div>
-                <Nav/>
-                <button onClick={() => {
-                    console.log(feed)
-                    console.log(userId)
-                    console.log(token)
-                    console.log(userData)
+      );
+      const postData = await response.json();
+      console.log(postData, "POSTDAATA");
+      replace(postData);
+    };
+    fetchThisComment().catch(console.error);
+  };
+  const fetchMore = async () => {
+    const response = await fetch(`http://localhost:8080?skip=${feed.length}`, {
+      headers: { Authorization: "Bearer " + token },
+    });
+    const postData = await response.json();
+    setFeed([...feed, ...postData.postList]);
+    console.log("runfetchmore");
+  };
 
-                }}>CONSOLE LOGS</button>
-                <div className='flex-container'>
-                    <Sidebar fullName={userData.first_name + ' ' + userData.last_name} />
-                    <div className='d-flex px-4'>
-                        <div className='d-flex flex-column'>
-                            <CreatePost loadFeed={loadFeed} token={token} userId={userId} />
-                                <div>
-                                    {feed ? feed.map((postInfo) => {
-                                        return (
-                                            <Post loadFeed={loadFeed} token={token} userId={userId} postInfo={postInfo} key={postInfo._id} showComment={showComment} />
-                                        )
-                                    }) : <h1>Loading</h1>}
-                                </div>
-                        </div>
+  return (
+    <>
+      {token ? (
+        <div>
+          <Nav />
+          <button
+            onClick={() => {
+              console.log(feed);
+              console.log(userId);
+              console.log(token);
+              console.log(userData);
+            }}
+          >
+            CONSOLE LOGS
+          </button>
+          <div className="flex-container">
+            <Sidebar
+              fullName={userData.first_name + " " + userData.last_name}
+            />
+            <div className="d-flex px-4">
+              <div className="d-flex flex-column">
+                {/* <CreatePost loadFeed={loadFeed} token={token} userId={userId} /> */}
+                <div className="container border bg-white rounded-4 create-post-width p-0 mb-3">
+                  <div className="d-flex post-head">
+                    <div className="pe-1">
+                      <Profile className="profile" />
                     </div>
-                    <Contacts />
+                    <div
+                      className="rounded-5 create-post-font cpcol"
+                      onClick={() => setPostModal(true)}
+                    >
+                      What's on your mind?
+                    </div>
+                  </div>
                 </div>
-            </div> : <Login />}
-        </>
-    )
-}
+                {postModal && (
+                  <CreatePost
+                    fullName={userData.first_name + " " + userData.last_name}
+                    loadFeed={loadFeed}
+                    token={token}
+                    userId={userId}
+                    setPostModal={setPostModal}
+                    setFeed={setFeed}
+                  />
+                )}
+                <button onClick={fetchMore}>WAZZUP</button>
+                <div id="scroll">
+                  <InfiniteScroll dataLength={feed.length} next={fetchMore} hasMore={true}>
+                    {feed ? (
+                      feed.map((postInfo) => {
+                        return (
+                          <Post
+                            setFeed={setFeed}
+                            loadFeed={loadFeed}
+                            token={token}
+                            userId={userId}
+                            postInfo={postInfo}
+                            key={postInfo._id}
+                            showComment={showComment}
+                          />
+                        );
+                      })
+                    ) : (
+                      <h1>Loading</h1>
+                    )}
+                  </InfiniteScroll>
+                </div>
+              </div>
+            </div>
+            <Contacts />
+          </div>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </>
+  );
+};
 
-export default Home
+export default Home;
