637be05cca05aa5db0e170e931c88c6e7a09bf05
agapdag g <agapdag@gmail.com>
2022-09-18 15:49:28 +0800

    Update routes

--- client/src/RouteSwitch.js
+++ client/src/RouteSwitch.js
@@ -1,42 +1,68 @@
-import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom"
-import Home from "./components/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import Login from "./components/Login";
-import Register from "./components/Register"
+import Register from "./components/Register";
 import { useState, useEffect } from "react";
-import Nav from "./components/Nav"
+import VisitUser from "./components/VisitUser";
+import Friends from "./components/Friends";
 
 const RouteSwitch = () => {
-    const [token, setToken] = useState('')
-    const [userId, setuserId] = useState('')
-    const [ps, sp] = useState('')
-    const [userData, setUserData] = useState({})
-    
-    const getUserInfo = async () => {
-        const response = await fetch('http://localhost:8080/users/' + userId, {
-            headers: { Authorization: 'Bearer ' + token }
-        })
-        const user = await response.json()
-        setUserData(user)
+  const [token, setToken] = useState("");
+  const [userId, setuserId] = useState("");
+  const [userData, setUserData] = useState({});
+
+  const getUserInfo = async () => {
+    const response = await fetch("http://localhost:8080/users/" + userId, {
+      headers: { Authorization: "Bearer " + token },
+    });
+    const user = await response.json();
+    setUserData(user);
+  };
+
+  useEffect(() => {
+    if (token) {
+      getUserInfo();
     }
+  }, [token]);
 
-    useEffect(() => {
-        console.log('asdaiosdaoijs')
-        if(token) {
-            getUserInfo()
-        }
-    }, [token])
-
-    return (
-        <BrowserRouter>
-                <Nav/>
-            <Routes>
-                <Route path="/" element={<Home userId={userId} token={token} ps={ps} userData={userData} />} />
-                <Route path="/reg" exact element={<Register />} />
-                <Route path="/login" exact element={<Login token={token} setToken={setToken} setuserId={setuserId} ps={ps} sp={sp}  />} />
-                {/* <Route path="/shop/:name" element={<Product shopList={shopList} setshopList={setshopList}/>} /> */}
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home userId={userId} token={token} userData={userData} />}
+        />
+        <Route
+          path="/reg"
+          exact
+          element={<Register setToken={setToken} setuserId={setuserId} />}
+        />
+        <Route
+          path="/login"
+          exact
+          element={
+            <Login token={token} setToken={setToken} setuserId={setuserId} />
+          }
+        />
+        <Route
+          path="/:userId"
+          element={
+            <VisitUser
+              userId={userId}
+              token={token}
+              userData={userData}
+              setUserData={setUserData}
+            />
+          }
+        />
+        <Route
+          path="/friends"
+          exact
+          element={<Friends token={token} userId={userId} />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default RouteSwitch;
