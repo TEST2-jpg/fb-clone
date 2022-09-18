cc476e19441680246e718802c50000e08b5148f1
agapdag g <agapdag@gmail.com>
2022-09-18 15:42:47 +0800

    Prevent btn click while in fetch

--- client/src/components/FriendAdd.js
+++ client/src/components/FriendAdd.js
@@ -17,6 +17,7 @@ const FriendAdd = ({token, userId, id, load}) => {
     }, [])
 
     const addFriend = async () => {
+        setIsLoading(prev => ({...prev, addFriend: true, undo: false}))
         const response = await fetch(`http://localhost:8080/friend/addFriend`, {
             method: 'POST',
             headers: {
@@ -34,6 +35,7 @@ const FriendAdd = ({token, userId, id, load}) => {
     }
 
     const undoRequest = async() => {
+        setIsLoading(prev => ({...prev, undo: true, addFriend: false}))
         const response = await fetch(`http://localhost:8080/friend/undoRequest`, {
             method: 'POST',
             headers: {
@@ -51,6 +53,7 @@ const FriendAdd = ({token, userId, id, load}) => {
     }
     
     const acceptRequest = async() => {
+        setIsLoading(prev => ({...prev, accept: true}))
         const response = await fetch(`http://localhost:8080/friend/acceptRequest`, {
             method: 'POST',
             headers: {
@@ -66,11 +69,18 @@ const FriendAdd = ({token, userId, id, load}) => {
         load()
     }
 
+    const [isLoading, setIsLoading] = useState({
+        addFriend: false,
+        undo: false,
+        remove: false,
+        accept: false,
+    })
+
     const determineButton = () => {
-        if (statusInfo.status === 0) return <button className="btn btn-primary fw-semibold shadow-none w-100" onClick={addFriend}>Add Friend</button>
-        else if (statusInfo.status === 1) return <button className="btn btn-primary fw-semibold shadow-none w-100" onClick={undoRequest}>Pending...</button>
-        else if (statusInfo.status === 2) return <button className="btn btn-primary fw-semibold shadow-none w-100" onClick={acceptRequest}>Accept request</button>
-        else if (statusInfo.status === 3) return <div><button className="btn btn-primary fw-semibold shadow-none w-100" onClick={undoRequest}>Remove friend</button></div>
+        if (statusInfo.status === 0) return <button className={`btn btn-primary fw-semibold shadow-none w-100 ${isLoading.addFriend ? 'disabled' : null}`} onClick={addFriend}>Add Friend</button>
+        else if (statusInfo.status === 1) return <button className={`btn btn-primary fw-semibold shadow-none w-100 ${isLoading.undo ? 'disabled' : null}`} onClick={undoRequest}>Pending...</button>
+        else if (statusInfo.status === 2) return <button className={`btn btn-primary fw-semibold shadow-none w-100 ${isLoading.accept ? 'disabled' : null}`} onClick={acceptRequest}>Accept request</button>
+        else if (statusInfo.status === 3) return <div><button className={`btn btn-primary fw-semibold shadow-none w-100 ${isLoading.undo ? 'disabled' : null}`} onClick={undoRequest}>Remove friend</button></div>
         return <div>Not loaded</div>
     }
     
